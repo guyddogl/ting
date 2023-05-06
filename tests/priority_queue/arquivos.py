84d411805a7e81ea263b613d386461431099a78f
Guyddo <guyddogl@gmail.com>
2023-05-06 19:18:22 -0300

    Requirement 6

---
+++ tests/priority_queue/arquivos.py
@@ -0,0 +1,12 @@
+mock = [
+    {
+        "nome_do_arquivo": "test.txt",
+        "qtd_linhas": 1,
+        "linhas_do_arquivo": ["Um texto aqui"],
+    },
+    {
+        "nome_do_arquivo": "test2.txt",
+        "qtd_linhas": 28,
+        "linhas_do_arquivo": ["Um texto aqui"],
+    },
+]
--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,21 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from tests.priority_queue.arquivos import mock
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    test = PriorityQueue()
+
+    test.enqueue(mock[0])
+    assert len(test.high_priority) == 1
+
+    test.enqueue(mock[1])
+    assert len(test.regular_priority) == 1
+
+    assert test.search(1) == mock[1]
+
+    test.dequeue()
+    assert len(test.high_priority) == 0
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        test.search(3)
