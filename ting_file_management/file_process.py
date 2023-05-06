655cbb4f1e89907fa67fdff5d36aeba97454ab4f
Guyddo <guyddogl@gmail.com>
2023-05-06 18:08:30 -0300

    Requirement 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_path = ""
+
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            file_path = instance.search(index)
+
+    if file_path == "":
+        file = txt_importer(path_file)
+        lines = len(file)
+        result = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": lines,
+            "linhas_do_arquivo": file,
+        }
+        instance.enqueue(result)
+        print(f"{result}")
 
 
 def remove(instance):
