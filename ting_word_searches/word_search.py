9f9d02ad503e3f27692a304e9eb09ad1d0bc55f5
Guyddo <guyddogl@gmail.com>
2023-05-07 08:11:21 -0300

    Requirement 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for index in range(len(instance)):
+        file_data = instance.search(index)
+        lines = file_data["linhas_do_arquivo"]
+        file_name = file_data["nome_do_arquivo"]
+        occurrences = [
+            {"linha": index + 1}
+            for index in range(len(lines))
+            if word.lower() in lines[index].lower()
+        ]
+        if occurrences:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": occurrences,
+                }
+            )
+    return result
 
 
 def search_by_word(word, instance):
